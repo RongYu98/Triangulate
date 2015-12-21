b81d09b7b264b65787d91610c544b129c65459aa
RongYu <stuy.rong.yu@gmail.com>
2015-12-21 12:53:54 -0500

    just moved the function created to the util.py

--- util.py
+++ util.py
@@ -1,13 +1,6 @@
-from flask import Flask, render_template, request, jsonify, util
-import urllib2
-import json
-
-app = Flask(__name__)
-
-@app.route("/",methods=["GET","POST"])
-def main():
-    lat = 40.714224
-    long = -73.961452
+def numTo(lat, long):
+    #lat = 40.714224
+    #long = -73.961452
     key = "AIzaSyC1HeKfjwS4x0KYw_Wgl5-IxLBELfa4oO0"
     basic = """https://maps.googleapis.com/maps/api/geocode/json?latlng="""+str(lat)+","+str(long)+"""&key=""" + key
     
@@ -15,20 +8,15 @@ def main():
     result = request.read()
     result = json.loads(result)
     return jsonify(result)
-    
-    #return render_template("Artist.html",images=final,artist=artist,Tracks = Tracks)
+    #return result
 
-@app.route("/name")
-def nameToNumbers():
-    add = "1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+
+
+def nameTo(add):
+    #add = "1600+Amphitheatre+Parkway,+Mountain+View,+CA"
     key = "AIzaSyC1HeKfjwS4x0KYw_Wgl5-IxLBELfa4oO0"
     query = "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key="+key
     request = urllib2.urlopen(query)
     result = request.read()
     result = json.loads(result)
     return jsonify(result)
-
-if (__name__ == "__main__"):
-        app.debug = True
-        app.secret_key = "secret"
-        app.run(host='0.0.0.0', port=8000)
