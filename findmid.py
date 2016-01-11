38249a4ba3b95b43d23245b708e0532308459655
Jason Shin <jasonshin98@gmail.com>
2016-01-11 13:18:38 -0500

    more algorithms

--- findmid.py
+++ findmid.py
@@ -1,6 +1,9 @@
+from flask import Flask, render_template, request, jsonify
+import urllib2
+import json
 import math
 
-def minDist( coords ):
+def geoMin( coords ):
     totx = 0
     toty = 0
     for coord in coords:
@@ -18,3 +21,70 @@ def minDist( coords ):
     lat = math.atan2(z, hyp)
     finalcoord = lat, lon
     return finalcoord
+
+def twoPointDist(a, b, unit):
+    radEarth = 0
+    if unit == "imperial":
+        radEarth = 3959
+    elif unit == "metric":
+        radEarth = 6371
+    """
+    key = "AIzaSyC1HeKfjwS4x0KYw_Wgl5-IxLBELfa4oO0"
+    basic = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(a[0])+","+str(a[1])+"&destinations="+str(b[0])+","+str(b[1])+"&units="+unit+"&key="+key
+    request = urllib2.urlopen(basic)
+    result = request.read()
+    result = json.loads(result)
+    #print result
+    distance = result['rows'][0]['elements'][0]['distance']['text']
+    distance =  distance.split(" ")[0]
+    print distance
+    return float(distance)
+    """
+    aLat = math.radians(a[0])
+    aLong = math.radians(a[1])
+    bLat = math.radians(b[0])
+    bLong = math.radians(b[1])
+    latDiff = abs(aLat - bLat)
+    lonDiff = abs(aLong - bLong)
+    var1 = math.sin(latDiff / 2) ** 2 + math.cos(aLat) * math.cos(bLat) * math.sin(lonDiff / 2) ** 2
+    var2 = 2 * math.atan2(math.sqrt(var1), math.sqrt(1 - var1))
+    d = radEarth * var2
+    print d
+
+"""
+Possible Algorithm:
+     lat=asin(sin(lat1)*cos(d)+cos(lat1)*sin(d)*cos(tc))
+     IF (cos(lat)=0)
+        lon=lon1      // endpoint a pole
+     ELSE
+        lon=mod(lon1-asin(sin(tc)*sin(d)/cos(lat))+pi,2*pi)-pi
+     ENDIF
+
+def eightPoints( coord ):
+""" 
+
+"""
+In Progress:
+
+def minDist( coords, unit ):
+    currentPoint = geoMin( coords )
+    minDist = 0
+    for coord in coords:
+        minDist += twoPointDist(currentPoint, coord, unit )
+    for x in range(len(coords)):
+        tmpDist = 0
+        for y in range(x+1, len(coords)):
+            tmpDist += twoPointDist(coords[x], coords[y], unit )
+        if tmpDist < minDist:
+            minDist = tmpDist
+            currentPoint = coords[x]
+    testDist = 0;
+    if unit == "imperial":
+        testDist = 6225
+    if unit == "metric":
+        testDist = 10018        
+"""
+
+pointa = (40.769750, -73.740648)
+pointb = (40.7180139,-74.0160826)
+twoPointDist(pointa, pointb, "imperial")
