bdcca116170ad09c1f8bab91cb8ec3aa003c999d
Masa13 <masahero55@gmail.com>
2016-01-11 01:55:23 -0500

    updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 import urllib2
 import json
 import util
@@ -11,7 +11,7 @@ def verify():
     else:
         session['log'] == 'unverified'
         return False
-
+    
 @app.route("/",methods=["GET","POST"])
 def main():
     lat = 40.714224
@@ -66,21 +66,23 @@ def login():
         return render_template('login.html')
     if verify():
         return redirect(url_for('home'))
-        if request.method == "POST":
-            form = request.form
-            button = form['button']
-            if button == "Register":
-                return redirect(url_for("register"))
-            else:
-                uname = form['username']
+    if request.method == "POST":
+        form = request.form
+        button = form['button']
+        if button == "Register":
+            return redirect(url_for("register"))
+        else:
+            uname = form['username']
+            session['username'] = uname
+            pword = form['password']
+            if util.authenticate(uname,pword):
+                session['log'] = 'verified'
                 session['username'] = uname
-                pword = form['password']
-                if util.authenticate(uname, pword):
-                    session['log'] = 'verified'
-                    session['username'] = uname
-                    return redirect(url_for('home'))
-                else:
-                    return render_template('login.html',error="Incorrect Username or Password")
+                return redirect(url_for('home'))
+            else:
+                return render_template('login.html', error="Incorrect Username or Password")
+            
+                
 
 @app.route('/register',methods=["GET","POST"])
 def register():
@@ -98,12 +100,27 @@ def register():
             session['username'] = uname
             return redirect(url_for('home'))
         else:
-            return render_template('register.html',err="That username is taken")
+            return render_template('register.html',err="That username is taken!")
+
+@app.route('/home', methods=["GET","POST"])
+def home():
+    if verify():
+        user=''
+        if 'username' in session:
+            user=session['username']
+        else:
+            user = session['username'] = "Null"
+        return render_template('home.html', user=user, posts=util.gettitles())
+    return redirect(url_for("login"))
 
 
-    
-
-
+@app.route('/logout')
+def logout():
+    if verify():
+        session['log'] = "unverified"
+    session['action'] = "Logged Out!"
+    return redirect(url_for('login'))
+        
 if (__name__ == "__main__"):
         app.debug = True
         app.secret_key = "secret"
