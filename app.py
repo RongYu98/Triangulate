8fbcad284f1bd232a2d09574cadcbc4f904f0c75
Masa13 <masahero55@gmail.com>
2016-01-29 17:41:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("about.html")
+    return render_template("index.html")
 
 @app.route("/name")
 def nameToNumbers():
